d4bef5e23ab486dae3489aa23a54e685141be9a9
joshmenard <jmenard@sfsu.edu>
2026-03-18 14:24:21 -0700

    added text and color changing functionality to the visualizer

--- sketch.js
+++ sketch.js
@@ -1,22 +1,56 @@
 let song;
+let visualColor = ("white"); // Default to white
+let input;
+let amplitude;  
 
 function setup() {
   createCanvas(1920, 1080);
-  createFileInput(handleFile);
+  input = createFileInput(handleFile);
+  textSize(20);
+
+  input.position(10, 50);
   amplitude = new p5.Amplitude();
 }
 
-function handleFile(file) {
-    song = loadSound(file.data, () => {
+function handleFile(file) {  
+  song = loadSound(file.data, () => {
       song.play();
     });
   }
 
+function keyPressed() {
+  if (key === '1') {
+    visualColor = "red"; 
+  } else if (key === '2') {
+    visualColor = "orange"; 
+  } else if (key === '3') {
+    visualColor = "yellow"; 
+  } else if (key === '4') {
+    visualColor = "lightGreen"; 
+  } else if (key === '5') {
+    visualColor = "lime"; 
+  } else if (key === '6') {
+    visualColor = "cyan"; 
+  } else if (key === '7') {
+    visualColor = "skyblue";
+  } else if (key === '8') {
+    visualColor = "magenta"; 
+  } else if (key === '9') {
+    visualColor = "pink"; 
+  } else if (key === '0') {
+    visualColor = "white"; 
+  } 
+}
+
 function draw() {
   background(0);
-  fill("pink");
+  fill(255);
+  text("Upload an audio file to start the visualizer", 10, 30);
+  text("Use number keys to change color", 10, 100);
 
+  fill(visualColor);
   let vol = amplitude.getLevel();
 
   ellipse(960, 540, 960*(vol*2), vol * 540);
 }
+
